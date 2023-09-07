116e5b73b7afac8b25019d98056fcbd0b52c3b2d
Martyn Garcia <martyn.garcia@gmail.com>
2023-09-06 21:35:49 -0600

    initial bos deployment and placeholder

---
+++ bos-frontend/deploy-testnet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+json_data=$(cat ~/.near-credentials/testnet/monstersdev.testnet.json)
+BOS_SIGNER_PUBLIC_KEY=$(echo $json_data | jq -r '.public_key')
+BOS_SIGNER_PRIVATE_KEY=$(echo $json_data | jq -r '.private_key')
+
+bos components deploy monstersdev.testnet sign-as monstersdev.testnet network-config testnet sign-with-plaintext-private-key --signer-public-key "$BOS_SIGNER_PUBLIC_KEY" --signer-private-key "$BOS_SIGNER_PRIVATE_KEY" send
