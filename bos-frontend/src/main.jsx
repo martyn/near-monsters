8a9f5a74bfbc5a73e318a327626c3aa579d74bb3
Martyn Garcia <martyn.garcia@gmail.com>
2023-10-02 17:09:10 -0600

    wip main site with placeholders

--- bos-frontend/src/main.jsx
+++ bos-frontend/src/main.jsx
@@ -1,23 +1,62 @@
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  color: #FFFFFF;
+  margin-right: auto; // Pushes it to the left
+`;
+const Nav = styled.div`
+  display: flex;
+  align-items: center;
+
+  ul {
+    display: flex;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    
+    li {
+      margin: 0 10px;
+
+      a {
+        text-decoration: none;
+        color: #FFFFFF;
+        transition: color 0.3s;
+
+        &:hover {
+          color: #FFA500; // You can choose a different hover color
+        }
+      }
+    }
+  }
+`;
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(to right, #7B68EE, #6A5ACD); // Gradient similar to the screenshot
+  padding: 10px 50px;
+  box-shadow: 0 2px 10px var(--blackA7);
+`;
 const Header = () => (
-  <div className="header">
-    <div className="logo">NEAR Monsters</div>
-    <div>
+  <StyledHeader>
+    <Logo>NEAR Monsters</Logo>
+    <Nav>
       <ul>
         <li>
           <a href="#purchase-alpha">Purchase ALPHA</a>
         </li>
         <li>
-          <a href="#play-game">Play game</a>
+          <a href="#inventory">Inventory</a>
+        </li>
+        <li>
+          <a href="#play-game">Play</a>
         </li>
         <li>
           <a href="#marketplace">Marketplace</a>
         </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
       </ul>
-    </div>
-  </div>
+    </Nav>
+  </StyledHeader>
 );
 
 const MainSection = () => (
