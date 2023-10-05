8d3edbe52e54493662c8946f16c7b951d7fef033
Martyn Garcia <martyn.garcia@gmail.com>
2023-10-04 22:14:29 -0600

    initial inventory - show full set

--- bos-frontend/src/inventory.jsx
+++ bos-frontend/src/inventory.jsx
@@ -1,7 +1,13 @@
   const nftContract = (context.networkId === "mainnet") ? "..." : "dev-1693936211939-67386471331489";
   const fullSetList = Near.view(nftContract, "full_set_listing", {});
-  const fullSetListTest = Near.view(nftContract, "full_set_listing_test", {});
   console.log("Full set list", fullSetList);
-  console.log("Full set list test", fullSetListTest);
-  return <div>Inventory</div>;
+  return <div><ul>
+  {fullSetList.map((item) =>
+    (<li>
+      <label>{item.name} - {item.rarity}</label>
+      <img src={item.url} width={224}/>
+    </li>)
+  )}
+  </ul>
+  </div>;
 
