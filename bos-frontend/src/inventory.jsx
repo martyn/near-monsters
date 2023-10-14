8346d0b1c464eb175aaf8d0209f731bed64dbfe2
Martyn Garcia <martyn.garcia@gmail.com>
2023-10-13 23:11:22 -0600

    better inventory

--- bos-frontend/src/inventory.jsx
+++ bos-frontend/src/inventory.jsx
@@ -1,5 +1,9 @@
-  const nftContract = (context.networkId === "mainnet") ? "..." : "dev-1693936211939-67386471331489";
-  const fullSetList = Near.view(nftContract, "full_set_listing", {});
+const nftContract = (context.networkId === "mainnet") ? "..." : "dev-1693936211939-67386471331489";
+const fullSetList = Near.view(nftContract, "full_set_listing", {});
+const nftsOwned = Near.view(nftContract, "nft_tokens_for_owner", {account_id: context.accountId, limit:50000});//{from_index: (nftsOwned-5).toString(), limit:5});
+console.log("--", nftsOwned);
+console.log("++", fullSetList);
+State.init({error: null});
 
 const Container = styled.div`
   display: flex;
@@ -35,20 +39,61 @@ const CardImage = styled.img`
   width: 224px;
   height: auto;
 `;
+
+function setOwnedFilter(value) {
+  console.log("Set owned filter", value);
+}
+function setRarityFilter(value) {
+  console.log("Set rarity filter", value);
+}
 return (
-	<Container>
-		<FilterPane>
-			{/* Filters will go here */}
-		</FilterPane>
-		<CardGrid>
-			{fullSetList.map((item, index) => (
-				<Card key={index}>
-					<CardName>{item.name} - {item.rarity}</CardName>
-					<CardImage src={item.url} />
-					{/* Place to put how many copies the user owns */}
-					<div>Copies: {item.copies}</div>
-				</Card>
-			))}
-		</CardGrid>
-	</Container>
+  <Container>
+    <FilterPane>
+      <div>
+        <label>Owned:</label>
+        <select onChange={(e) => setOwnedFilter(e.target.value)} value={state.ownedFilter}>
+          <option value="all">All</option>
+          <option value="owned">Owned Only</option>
+        </select>
+      </div>
+      
+      <div>
+        <label>Rarity:</label>
+        <select onChange={(e) => setRarityFilter(e.target.value)} value={rarityFilter}>
+          <option value="All">All</option>
+          <option value="Common">Common</option>
+          <option value="Uncommon">Uncommon</option>
+          <option value="Rare">Rare</option>
+        </select>
+      </div>
+
+      <div>
+        <label>Type:</label>
+        {["Undead", "Mage", "Demon", "Beast", "Warrior", "Spirit", "Elemental", "Vampire", "Celestial"].map((type) => (
+          <div key={type}>
+            <input 
+              type="checkbox" 
+              checked={false}
+              onChange={() => {
+                //const newTypeFilter = typeFilter.includes(type)
+                //  ? typeFilter.filter(t => t !== type)
+                //  : [...typeFilter, type];
+                //setTypeFilter(newTypeFilter);
+              }}
+            />
+            {type}
+          </div>
+        ))}
+      </div>
+    </FilterPane>
+    <CardGrid>
+      {fullSetList.map((item, index) => (
+        <Card key={index}>
+          <CardName>{item.name} - {item.rarity}</CardName>
+          <CardImage src={item.url} />
+          <div>Copies Owned: {item.copies}</div>
+        </Card>
+      ))}
+    </CardGrid>
+  </Container>
 );
