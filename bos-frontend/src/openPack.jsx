3fcebdc728973f5cff6cdf7a0a3804a6a8b0b1b2
Martyn Garcia <martyn.garcia@gmail.com>
2023-10-14 23:21:50 -0600

    add reveal moment

--- bos-frontend/src/openPack.jsx
+++ bos-frontend/src/openPack.jsx
@@ -5,15 +5,19 @@ const isOpenDisabled = (alphaPacksOwned === 0);
 const nftsOwned = Near.view(nftContract, "nft_supply_for_owner", {account_id: context.accountId});
 const nfts = Near.view(nftContract, "nft_tokens_for_owner", {account_id: context.accountId, limit:1000});//{from_index: (nftsOwned-5).toString(), limit:5});
 const sortedNfts = nfts.sort((a, b) => {
-  const dateA = a.metadata?.issued_at || "0000-00-00";
-  const dateB = b.metadata?.issued_at || "0000-00-00";
+  const dateA = a.metadata.issued_at || "0000-00-00";
+  const dateB = b.metadata.issued_at || "0000-00-00";
   return dateB.localeCompare(dateA);
 });
 
 const revealNfts = sortedNfts.slice(0, 5);
 const purchaseLink = "https://test.near.org/monstersdev.testnet/widget/purchase";
 
-State.init({error: null});
+const lastOpened = Storage.privateGet("lastOpened");
+const allowReveal = (sortedNfts.length > 0) && (!lastOpened || (sortedNfts[0].metadata.issued_at.localeCompare(lastOpened)) < 0);
+
+State.init({error: null, allowReveal: allowReveal, reveal: [false, false, false, false, false]});
+
 const openPack = () => {
   try {
     Near.call(ftContract, 'open_pack', {}, 300000000000000, 10000000000000000000000);
@@ -22,6 +26,39 @@ const openPack = () => {
   }
 
 }
+
+
+const RevealableCard = ({ index, nft }) => {
+  const reveal = (index) => {
+    return () => {
+      let reveal = state.reveal;
+      reveal[index]=true;
+      //TODO: set storage if all revealed
+      State.update({ ...state, reveal: reveal });
+    }
+  }
+  const rarity = () => {
+    return JSON.parse(nft.metadata.extra).rarity;
+  }
+
+  return (
+    <li>
+      {
+        (!state.reveal[index]) ?
+          (
+            <button onClick={reveal(index)}>
+              Reveal
+            </button>
+          ) : (
+          <>
+            <img src={nft.metadata.media} width={278}/>
+            <p>{nft.metadata.title} {nft.metadata.description} {rarity()}</p>
+          </>
+        )
+      }
+    </li>
+  );
+};
 
 return (
   <div className="App">
@@ -37,11 +74,15 @@ return (
           </div>
         </div>
       </h3>
-      <ul>
-      {
-        revealNfts.map((nft) => <li><img src={nft.metadata.media} width={128}/>{nft.metadata.title} {nft.metadata.description} {nft.metadata.extra.rarity}</li>)
+      {state.allowReveal &&
+        <div><p>Your pack is ready</p>
+          <ul>
+            {revealNfts.map((nft, index) => (
+              <RevealableCard nft={nft} index={index} />
+            ))}
+          </ul>
+        </div>
       }
-      </ul>
       <div>
         <span class="text-decoration-underline">{nftsOwned}</span> Monster NFTs collected
       </div>
